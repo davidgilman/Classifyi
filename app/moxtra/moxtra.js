ef5ac4db15600e88f90ecd071777ef570e9e5b4f
Andrew George <KingsOfTyrants@gmail.com>
2016-05-01 05:22:59 -0700

    Adding containing html file. chatroom is incorrectly nesting inside iframe however.

--- app/moxtra/moxtra.js
+++ app/moxtra/moxtra.js
@@ -1,22 +1,5 @@
 const https = require('https');
 
-module.exports = {
-  hi : function() {
-    console.log("Hello");
-  },
-  init: function() {
-  var options = {
-        mode: "sandbox", //for production environment change to "production"
-        client_id: "mD_t2cBz6d0",
-        access_token: globalAccessToken, //valid access token from user authentication
-        invalid_token: function(event) {
-            alert("Access Token expired for session id: " + event.session_id);
-        }
-    };
-    Moxtra.init(options);
-  }
-}
-
 var options = {
   hostname: 'apisandbox.moxtra.com',
   port: 443,
@@ -29,8 +12,23 @@ var globalAccessToken;
 var req = https.request(options, function (res) {
   if (res.statusCode != 200) console.log("ERROR! CONNECTION BAD.");
   res.on('data', function (d) {
-  	globalAccessToken = d.access_token;
+    globalAccessToken = JSON.parse(d).access_token;
+    console.log(globalAccessToken);
   });
 });
 req.end();
 
+
+module.exports = {
+  init: function() {
+  var options = {
+        mode: "sandbox", //for production environment change to "production"
+        client_id: "mD_t2cBz6d0",
+        access_token: globalAccessToken, //valid access token from user authentication
+        invalid_token: function(event) {
+            alert("Access Token expired for session id: " + event.session_id);
+        }
+    };
+    //Moxtra.init(options);
+  }
+}
